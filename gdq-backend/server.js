22351d0cfe23d4a1709c2fa09f1a7953bf7c234f
Garrison Hinshaw <garrison.hinshaw18@gmail.com>
2023-01-15 11:37:30 -0500

    trying to fix database connection issue

--- gdq-backend/server.js
+++ gdq-backend/server.js
@@ -1,16 +1,32 @@
-const express = require('express')
-const app = express()
-
 require('dotenv').config()
-app.use(express.json())
+const express = require('express')
+const methodOverride = require('method-override')
+const app = express();
+const router = require('../controllers/')
+//const path = require('path')
+
+// Express Settings
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
+app.use(express.json)
 app.use(express.urlencoded({ extended: false }))
+//app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
-    res.status(200).json({
-        message: 'Welcome to login'
-    })
+    res.json('home')
 })
 
+app.use(express.urlencoded({ extended: true }))
+
+//app.use('/places', require('./controllers/'))
+//app.get('*', (req, res) => {
+  //  res.render('error404')
+//})
+
+//app.use(express.static(path.join(__dirname, '../gdq-frontend/build', 'build')))
+
 app.listen(process.env.PORT, () => {
-    console.log(`🎸 Rockin' on port: ${process.env.PORT}`)
+  console.log("I am alive port " + process.env.PORT)
 })
