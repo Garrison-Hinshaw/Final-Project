b2b60960fa6565c36181e6ff568b6b03fde067a3
danielf9403 <103592213+danielf9403@users.noreply.github.com>
2023-01-22 10:30:20 -0500

    sign in page

--- gdq-frontend/src/pages/Home.jsx
+++ gdq-frontend/src/pages/Home.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 
+import { Container, Row, Col } from 'react-bootstrap';
+
 export default function Home () {
 return (
     <div> 
-    
-    hr
+        <Container>
+            <Row>
+              <Col xs={6} md={4}>
+                <img src={"gdq-frontend/src/assets/PhotosForCode/1. PngItem_1171479.png"} alt="image1" />
+              </Col>
+              <Col xs={6} md={4}>
+                <img src={"../assets/PhotosForCode/PngItem_2111340.png"} alt="image2" />
+              </Col>
+              <Col xs={6} md={4}>
+                <img src={"../assets/PhotosForCode/1. PngItem_1171479.png"} alt="image3" />
+              </Col>
+            </Row>
+        </Container>
     
     </div>
 
