f4e9e150d6cc5f3704552b8adba3393168b6e0ba
danielf9403 <103592213+danielf9403@users.noreply.github.com>
2023-01-22 14:36:06 -0500

    pull req

--- gdq-frontend/src/pages/Home.jsx
+++ gdq-frontend/src/pages/Home.jsx
@@ -3,18 +3,13 @@ import { Container, Row, Col } from 'react-bootstrap';
 import NewProduct from "./NewProduct";
 
 
-
-import { Container, Row, Col } from 'react-bootstrap';
-
-import { Container, Row, Col } from 'react-bootstrap';
-
 export default function Home () {
 return (
     <div> 
         <Container>
             <Row>
               <Col xs={6} md={4}>
-                <img src={"gdq-frontend/src/assets/PhotosForCode/1. PngItem_1171479.png"} alt="image1" />
+                <img src={"gdq-frontend/src/assets/PhotosForCode/nike-pic.webp"} alt="image1" />
               </Col>
               <Col xs={6} md={4}>
                 <img src={"../assets/PhotosForCode/PngItem_2111340.png"} alt="image2" />
