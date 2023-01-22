fa9d7f4f4966b914972b45f7f2fa142888379d30
Garrison Hinshaw <103464316+Garrison-Hinshaw@users.noreply.github.com>
2023-01-22 12:39:28 -0500

    Update Home.jsx

--- gdq-frontend/src/pages/Home.jsx
+++ gdq-frontend/src/pages/Home.jsx
@@ -1,23 +1,28 @@
 import React from "react";
+import { Container, Row, Col } from 'react-bootstrap';
+import NewProduct from "./NewProduct";
 
 
 
+import { Container, Row, Col } from 'react-bootstrap';
 
 export default function Home () {
-    return (
-        <div> 
-            
-            
-        
-        </div>
-    
-    )
-    
-    
+return (
+    <div> 
+        <Container>
+            <Row>
+              <Col xs={6} md={4}>
+                <img src={"gdq-frontend/src/assets/PhotosForCode/1. PngItem_1171479.png"} alt="image1" />
+              </Col>
+              <Col xs={6} md={4}>
+                <img src={"../assets/PhotosForCode/PngItem_2111340.png"} alt="image2" />
+              </Col>
+              <Col xs={6} md={4}>
+                <img src={"../assets/PhotosForCode/1. PngItem_1171479.png"} alt="image3" />
+              </Col>
+            </Row>
+        </Container>
+    </div>
+)
     }
     
-        
-
-
-
-
