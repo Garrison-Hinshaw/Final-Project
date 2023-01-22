5245443cbbccd22920e7298736d0a2e870a8b18b
Garrison Hinshaw <103464316+Garrison-Hinshaw@users.noreply.github.com>
2023-01-22 12:34:46 -0500

    Update App.js

--- gdq-frontend/src/App.js
+++ gdq-frontend/src/App.js
@@ -5,13 +5,17 @@ import  Home  from "../src/pages/Home";
 import NewProduct  from "./pages/NewProduct";
 import  Contact  from "./pages/Contact";
 import  AboutUs  from "./pages/AboutUs";
+// import { login } from "./components/login";
 import NavBar from "./components/navBar";
 import SiteTitle from "./components/SiteTitle";
+// import { createBrowserRouter, createRoutesFromElements, Route, Link, Outlet, RouterProvider } from "react-router-dom";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import  Product from "../src/pages/Productt";
 import  PRODUCTS  from "./pages/Product";
 import { EditPro } from "./pages/EditPro";
-
+import login from "./pages/login";import "bootstrap/dist/css/bootstrap.min.css"
+import loginPage from "./components/login";
+import Auth from "./components/login"
 
 export default function App() {
 
@@ -20,6 +24,7 @@ export default function App() {
       <div className="App">
         <Router> 
         <SiteTitle />
+       <login/>
         <NavBar />
        <br/>
           <Routes>
@@ -27,7 +32,8 @@ export default function App() {
             <Route path="/contact" element={<Contact/>} />
             <Route path="/newProduct" element={<NewProduct/>} />
             <Route path="/aboutUs" element={<AboutUs/>} />
-
+            <Route path="/login" element={<Auth />} />
+            <Route path="/loginPage" element={<loginPage />} />
           </Routes>
         </Router>
         {/* <RouterProvider router={router} /> */}
